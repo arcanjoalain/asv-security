dbbb8cbf36b39d7450653d515f65cab3b3a443f7
Alain S Vieira <alain.vieira@institutoitn.com.br>
2021-03-05 10:57:26 -0400

    clean to rebase

--- src/main/java/br/com/asv/security/ms/controller/INewUserController.java
+++
@@ -1,8 +0,0 @@
-package br.com.asv.security.ms.controller;
-
-import br.com.asv.security.ms.dto.IApplicationUser;
-
-public interface INewUserController<I> {
-
-	void save(IApplicationUser<I> user);
-}
