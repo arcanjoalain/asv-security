dd4105db4aa9a31d1ff555c2054edd7bdc6244da
Alain de Souza Vieira <arcanjoalain@gmail.com>
2021-08-23 12:36:36 -0400

    Update ABaseSecurityConfig.java

--- src/main/java/br/com/asv/security/controller/ABaseSecurityConfig.java
+++ src/main/java/br/com/asv/security/controller/ABaseSecurityConfig.java
@@ -16,84 +16,88 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.asv.security.constant.SecurityConstants;
 import br.com.asv.security.dto.IApplicationUser;
 import br.com.asv.security.filter.AJWTAuthenticationFilter;
 import br.com.asv.security.filter.JWTAuthorizationFilter;
 import lombok.Getter;
-public abstract class ABaseSecurityConfig<
-	U extends User,
-	E extends IApplicationUser<I>,
-	F extends AJWTAuthenticationFilter<E,I>,I> 
-		extends WebSecurityConfigurerAdapter implements IBaseSecurityConfig<E,F,I> {
-    private IBaseUserDetailService<I,U> userDetailsService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    
+
+public abstract class ABaseSecurityConfig<U extends User, E extends IApplicationUser<I>, F extends AJWTAuthenticationFilter<E, I>, I>
+		extends WebSecurityConfigurerAdapter implements IBaseSecurityConfig<E, F, I> {
+	private IBaseUserDetailService<I, U> userDetailsService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 //    @Getter
 //    private String urlPublic = "/auth/login/**";
-    
-    @Getter
-    private SecurityConstants securityConstants;
 
-    public ABaseSecurityConfig(IBaseUserDetailService<I,U> userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        initialize();
+	@Getter
+	private SecurityConstants securityConstants;
+
+	public ABaseSecurityConfig(IBaseUserDetailService<I, U> userDetailsService,
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		initialize();
 //        this.securityConstants.setLoginUrl(this.urlPublic);
-    }
-    
-    public ABaseSecurityConfig(IBaseUserDetailService<I,U> userDetailsService, 
-    		BCryptPasswordEncoder bCryptPasswordEncoder, String urlPublic) {
-    	this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        initialize();
+	}
+
+	public ABaseSecurityConfig(IBaseUserDetailService<I, U> userDetailsService,
+			BCryptPasswordEncoder bCryptPasswordEncoder, String urlPublic) {
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		initialize();
 //        this.urlPublic = urlPublic;
-        this.securityConstants.setLoginUrl(urlPublic);
-    }
-    
-    private void initialize() {
-    	this.securityConstants = new SecurityConstants();
-    }
-    
-    private RequestMatcher prepareRquestPublic() {
-  	  List<RequestMatcher> publicUrl = new LinkedList<>();
-  	  for (String urlPublicInt : getPublicUrls()) {
-  		  publicUrl.add(new AntPathRequestMatcher(urlPublicInt));
-  	 }
-  	  return new OrRequestMatcher(publicUrl);
-    }
-    
-    @Override
-    public void configure(final WebSecurity web) {
-      web.ignoring().requestMatchers(prepareRquestPublic());
+		this.securityConstants.setLoginUrl(urlPublic);
+	}
+
+	private void initialize() {
+		this.securityConstants = new SecurityConstants();
+	}
+
+	private RequestMatcher prepareRquestPublic() {
+		List<RequestMatcher> publicUrl = new LinkedList<>();
+		for (String urlPublicInt : getPublicUrls()) {
+			publicUrl.add(new AntPathRequestMatcher(urlPublicInt));
+		}
+		return new OrRequestMatcher(publicUrl);
+	}
+
+	@Override
+	public void configure(final WebSecurity web) {
+		web.ignoring().requestMatchers(prepareRquestPublic());
 //      web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**")
-      web.ignoring().antMatchers(HttpMethod.OPTIONS)
-  	.antMatchers(HttpMethod.OPTIONS, this.securityConstants.getLoginUrl())
-  	.antMatchers(HttpMethod.POST, this.securityConstants.getLoginUrl())
- 	.antMatchers(HttpMethod.POST, "/auth/**")
-  	.antMatchers(HttpMethod.GET, "/ws/file/**")
-  	.antMatchers("/ws/translate/**")
-  	.antMatchers("/assets/**", "/webjars/**", "/api-docs/**")
-  	.antMatchers("/jsondoc/**", "/jsondoc-ui.html", "/swaggerui", "/swagger-ui.html", "/swagger-ui/*", "/swagger-resources/**", "/v2/api-docs");
-    }
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	F filterAuth = createFilterAuth();
-    	filterAuth.setFilterProcessesUrl(this.securityConstants.getLoginUrl());
-    	
-        http.cors().and().csrf().disable().authorizeRequests()
-        		.antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers(this.securityConstants.getLoginUrl()).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(filterAuth)
-                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-                // this disables session creation on Spring Security
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-    
+		web.ignoring().antMatchers(HttpMethod.OPTIONS)
+				.antMatchers(HttpMethod.OPTIONS, this.securityConstants.getLoginUrl())
+				.antMatchers(HttpMethod.POST, this.securityConstants.getLoginUrl())
+				.antMatchers(HttpMethod.POST, "/auth/**").antMatchers(HttpMethod.GET, "/ws/file/**")
+				.antMatchers("/ws/translate/**").antMatchers("/assets/**", "/webjars/**", "/api-docs/**")
+				.antMatchers("/jsondoc/**", "/jsondoc-ui.html", "/swaggerui", "/swagger-ui.html", "/swagger-ui/*",
+						"/swagger-resources/**", "/v2/api-docs");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		F filterAuth = createFilterAuth();
+		filterAuth.setFilterProcessesUrl(this.securityConstants.getLoginUrl());
+
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll()
+				.antMatchers(this.securityConstants.getLoginUrl()).permitAll().anyRequest().authenticated().and()
+				.addFilter(filterAuth).addFilter(new JWTAuthorizationFilter(authenticationManager()))
+				// this disables session creation on Spring Security
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
 //    @Bean
 //    CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration configuration = new CorsConfiguration();
@@ -108,7 +112,7 @@ public abstract class ABaseSecurityConfig<
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
 //    }
-    
+
 //    @Bean
 //	public CorsFilter corsFilter() {
 //		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -126,16 +130,14 @@ public abstract class ABaseSecurityConfig<
 //		return new CorsFilter(source);
 //	}
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-    
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-    
-    
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 }
